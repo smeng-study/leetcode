d15a0837460579bb80e7dd5ded4968467d960904
smeng <ivy.si.meng@gmail.com>
2016-10-06 13:59:38 -0500

    reverse string update

--- lcode/src/lcode/ReverseString.java
+++ lcode/src/lcode/ReverseString.java
@@ -6,11 +6,23 @@ package lcode;
  * 
  */
 
-public class ReverseString {
+// Write a function that takes a string as input and returns the string reversed.
+//
+// Example:
+// Given s = "hello", return "olleh".
 
+public class ReverseString {
+	public static String reverseString(String s) {
+		if(s==null || s.length()==0) {
+			return s;
+		}
+		StringBuilder sb = new StringBuilder(s);
+		return sb.reverse().toString();
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		String s = "asdafe";
+		System.out.println(reverseString(s));
 	}
 
 }
