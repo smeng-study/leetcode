7e76eadc1d9648d09536eb8b6afd567cb02de2e4
smeng <ivy.si.meng@gmail.com>
2016-10-14 09:59:02 -0500

    power of two bitwise operation

--- lcode/src/lcode/PowerofTwo.java
+++ lcode/src/lcode/PowerofTwo.java
@@ -17,12 +17,15 @@ public class PowerofTwo {
 	public static boolean isPowerOfTwo2(int n) {
 		return Math.log10(n)/Math.log10(2) % 1 == 0;
     }
+	public static boolean isPowerOfTwo3(int n) {
+		return n>0 && (n&(n-1))==0;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num1 = 2312;
 		int num2 = 256;
-		System.out.println(isPowerOfTwo2(num1));
-		System.out.println(isPowerOfTwo2(num2));
+		System.out.println(isPowerOfTwo3(num1));
+		System.out.println(isPowerOfTwo3(num2));
 	}
 
 }
